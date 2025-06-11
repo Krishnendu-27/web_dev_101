80066aff67f7b4662235d20ab2701d47d7ec9496
Krishnendu Ghosh <kai27deep03@gmail.com>
2025-06-11 22:28:36 +0530

    exerice of js learning

---
+++ _03-JavaScript/_00-practice/_00-script.js
@@ -0,0 +1,13 @@
+/*
+1.
+Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+Log their values to the // console.
+*/
+let country = "india";
+let continent = "Asia";
+let population = "140Cr";
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
